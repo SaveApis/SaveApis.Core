e3bb80f9c1c426a2d9cd923e0efa244954d86ee6
SaveApis | Felix Hake <felix.hake@saveapis.com>
2024-07-01 19:17:06 +0200

    feat(modules): Create Mediator module
    Create `MediatorModule.cs`
    
    Author: Felix Hake

---
+++ SaveApis.Core/Application/DI/MediatorModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using SaveApis.Core.Application.Extensions;
+using SaveApis.Core.Infrastructure.Events.Interfaces;
+
+namespace SaveApis.Core.Application.DI;
+
+public class MediatorModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssemblies(WebApplicationBuilderExtension.Assemblies.Distinct()
+                .ToArray()));
+
+        builder.Populate(collection);
+
+        builder.RegisterAssemblyTypes(WebApplicationBuilderExtension.Assemblies.Distinct().ToArray())
+            .Where(t => t.GetInterfaces().Any(i => i.IsAssignableTo(typeof(IEvent)))).AsImplementedInterfaces();
+    }
+}
