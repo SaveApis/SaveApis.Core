cab2f95798b973cb8acd26a33e17a76821b8cf9e
SaveApis | Felix Hake <felix.hake@saveapis.com>
2024-07-01 18:11:14 +0200

    feat(modules): Create serilog module
    Create `SerilogModule.cs`
    
    Author: Felix Hake

---
+++ SaveApis.Core/Application/DI/SerilogModule.cs
@@ -0,0 +1,29 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace SaveApis.Core.Application.DI;
+
+public class SerilogModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+        collection.AddSerilog(configuration =>
+        {
+            configuration
+                .Enrich.FromLogContext()
+                .WriteTo.Console(
+                    outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}",
+                    theme: AnsiConsoleTheme.Code, applyThemeToRedirectedOutput: true)
+                .WriteTo.File("Log/log.txt", rollingInterval: RollingInterval.Day,
+                    outputTemplate:
+                    "[{Timestamp:HH:mm:ss} {Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}");
+        });
+
+        builder.Populate(collection);
+    }
+}
