8167934d25601b1704dae26709ab0b6f9b4bce49
SaveApis | Felix Hake <felix.hake@saveapis.com>
2024-07-01 19:19:35 +0200

    feat(extensions): Add extension methods
    Create `WebApplicationExtension.cs`
    
    Update example project
    
    Author: Felix Hake

---
+++ SaveApis.Core/Application/Extensions/WebApplicationExtension.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SaveApis.Core.Application.Events;
+
+namespace SaveApis.Core.Application.Extensions;
+
+public static class WebApplicationExtension
+{
+    public static async Task RunSaveApisAsync(this WebApplication application)
+    {
+        var mediator = application.Services.CreateScope().ServiceProvider.GetRequiredService<IMediator>();
+
+        await application.StartAsync();
+        await mediator.Publish(new ApplicationStartedEvent());
+        await application.WaitForShutdownAsync();
+        await mediator.Publish(new ApplicationStoppedEvent());
+    }
+}
