67b6abc17f54300733f4469127c2f9535e8d60f6
SaveApis | Felix Hake <felix.hake@saveapis.com>
2024-07-02 18:53:54 +0200

    feat(modules): Create AutoMapper module
    Create `AutoMapperModule.cs`
    
    Update example project
    
    Author: Felix Hake

---
+++ Example/SaveApis.Core.Example/Application/Mapper/ExampleMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SaveApis.Core.Example.Application.Models;
+using SaveApis.Core.Example.Application.Models.DTO;
+
+namespace SaveApis.Core.Example.Application.Mapper;
+
+public class ExampleMapperProfile : Profile
+{
+    public ExampleMapperProfile() : base("Example Mapper Profile")
+    {
+        CreateMap<ExampleMapperModel, ExampleMapperDto>();
+    }
+}
---
+++ SaveApis.Core/Application/DI/AutoMapperModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using AutoMapper.Contrib.Autofac.DependencyInjection;
+using SaveApis.Core.Application.Extensions;
+
+namespace SaveApis.Core.Application.DI;
+
+public class AutoMapperModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterAutoMapper(false, WebApplicationBuilderExtension.Assemblies.ToArray());
+    }
+}
