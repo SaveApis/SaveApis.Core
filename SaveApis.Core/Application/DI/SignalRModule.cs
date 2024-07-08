f4397547610d59a4815ee9af7524c155b066eab1
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2024-07-08 18:31:23 +0200

    feat(modules): SignalR Module (#27)
    Create `SignalRModule.cs`
    
    Update example project
    
    Author: Felix Hake

---
+++ SaveApis.Core/Application/DI/SignalRModule.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SaveApis.Core.Application.DI;
+
+public class SignalRModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        var collection = new ServiceCollection();
+
+        collection.AddSignalR();
+
+        builder.Populate(collection);
+    }
+}
