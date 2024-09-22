1353024277e9df26aa28a77e4f93703654e81187
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2024-09-22 19:10:02 +0200

    feat: Default regex generator (#52)
    Author: Felix Hake

---
+++ SaveApis.Core/Infrastructure/DefaultRegexNames.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SaveApis.Core.Infrastructure;
+
+public partial class DefaultRegexNames
+{
+    [GeneratedRegex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+    public static partial Regex Email();
+}
