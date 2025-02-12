6c142ba62042f96886e31d86c3b5e61a022d3c8e
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2025-02-12 21:04:03 +0100

    feat(common): Add tracking interface (#103)
    Author: Felix Hake

---
+++ src/SaveApis.Core.Common/Infrastructure/Persistence/Sql/Entity/ITrackedEntity.cs
@@ -0,0 +1,10 @@
+﻿using SaveApis.Core.Common.Domain.VO;
+
+namespace SaveApis.Core.Common.Infrastructure.Persistence.Sql.Entity;
+
+public interface ITrackedEntity
+{
+    Id Id { get; }
+
+    ICollection<Tuple<string, string?, string?>> GetChanges();
+}
