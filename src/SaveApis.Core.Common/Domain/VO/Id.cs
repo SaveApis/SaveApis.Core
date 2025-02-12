625948d4a372441a16b67243c6341e948d43a003
SaveApis | Felix Hake <felix.hake@saveapis.com>
2025-02-12 21:01:10 +0100

    feat(common): Add tracking interface
    Author: Felix Hake

---
+++ src/SaveApis.Core.Common/Domain/VO/Id.cs
@@ -0,0 +1,21 @@
+﻿using Vogen;
+
+namespace SaveApis.Core.Common.Domain.VO;
+
+[ValueObject<Guid>(conversions: Conversions.NewtonsoftJson | Conversions.EfCoreValueConverter)]
+public partial class Id
+{
+    public static readonly Id Empty = new Id(Guid.Empty);
+
+    private static Guid NormalizeInput(Guid input)
+    {
+        return input;
+    }
+
+    private static Validation Validate(Guid input)
+    {
+        return input == Guid.Empty
+            ? Validation.Invalid("Id cannot be empty")
+            : Validation.Ok;
+    }
+}
